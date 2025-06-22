3422e46c6964eaba85a3568ae729706c1d104845
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-06-22 19:30:28 +0500

    updating admin/layout for getting user from authStore

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,39 +1,36 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { observer } from 'mobx-react-lite'
+import { useEffect } from 'react'
 import { useRouter, usePathname } from 'next/navigation'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '@/firebase/client'
 import Sidebar from '@/components/Sidebar'
+import { authStore } from '@/store' // импортируем экземпляр
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState(true)
-  const [authenticated, setAuthenticated] = useState(false)
+const AdminLayout = observer(({ children }: { children: React.ReactNode }) => {
   const router = useRouter()
   const currentPath = usePathname()
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setAuthenticated(true)
-      } else {
-        if (currentPath !== '/admin/login') {
-          router.push('/admin/login')
-        }
-      }
-      setLoading(false)
-    })
+    // Теперь используем экземпляр authStore, а не класс AuthStore
+    if (!authStore.isLoading && !authStore.isAuthenticated && currentPath !== '/admin/login') {
+      router.push('/admin/login')
+    }
+  }, [authStore.isLoading, authStore.isAuthenticated, currentPath, router])
 
-    return () => unsubscribe()
-  }, [router, currentPath])
+  if (authStore.isLoading) {
+    return <div className="p-8">Проверка пользователя...</div>
+  }
 
-  if (loading) return <div className="p-8">Проверка пользователя...</div>
-  if (!authenticated && currentPath !== '/admin/login') return null
+  if (!authStore.isAuthenticated && currentPath !== '/admin/login') {
+    return null
+  }
 
   return (
     <div className="flex gap-4 h-[100vh] p-4 bg-neutral-100">
-      {authenticated && currentPath !== '/admin/login' && <Sidebar />}
-      <main className="flex-1 overflow-y-auto ">{children}</main>
+      {authStore.isAuthenticated && currentPath !== '/admin/login' && <Sidebar />}
+      <main className="flex-1 overflow-y-auto">{children}</main>
     </div>
   )
-}
+})
+
+export default AdminLayout
